fa661a45fd36e70e527aaf65828a4e48026e627d
tago <78416380+t-a-g-o@users.noreply.github.com>
2024-04-18 21:05:33 -0700

    fixing for ubuntu/linux

--- assets/watcher.py
+++ assets/watcher.py
@@ -5,38 +5,12 @@ from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.backends import default_backend
 from cryptography.fernet import Fernet
 
-# Directory to clone the repo to
-clonerepoat = '/Users/' + os.getlogin() + '/Documents/GitHub/'
 
 #-------------------------------------------------------------------------------------
 #              ONLY CHANGE IF YOU KNOW WHAT YOU ARE DOING!!
-#     https://github.com/tagoWorks/akod/wiki/AKoD-Encryption-Variables
 #-------------------------------------------------------------------------------------
 
 
-
-def clone(url, pat):
-    git_command = ['git', 'clone', f'https://{pat}@{url}']
-    try:
-        if not os.path.exists(clonerepoat):
-            os.makedirs(clonerepoat)
-        os.chdir(clonerepoat)
-        output = subprocess.check_output(git_command)
-        os.chdir(licensestorage_repo)
-        if os.path.exists('index.html'):
-            os.remove('index.html')
-        url = 'https://cdn.tago.works/apps/akod/webpage/index.html'
-        response = requests.get(url)
-        content = response.text
-        new = content.replace('https://githublink.com/', f'https://github.com/{username}/{licensestorage_repo}')
-        with open('index.html', 'w') as f:
-            f.write(new)
-            f.close()
-        commit_changes('Reading and Writing OK')
-    except subprocess.CalledProcessError as e:
-        print(f"Error: {e}")
-    print("Repository cloned successfully.")
-    exit()
 def encrypt_file_pass(file_path):
     global privatekey
     with open(file_path, 'rb') as f:
@@ -64,7 +38,7 @@ def encrypt_file_pass(file_path):
 def process_new_folders(new_folders):
     for folder in new_folders:
         folder_path = os.path.join(registered_accounts, folder)
-        target_folder_path = os.path.join(repo, folder)
+        target_folder_path = os.path.join(webdavdirectory, folder)
         if not os.path.exists(target_folder_path):
             try:
                 def ignore_password_files(dir, contents):
@@ -78,34 +52,21 @@ def process_new_folders(new_folders):
                         encrypted_file_path = os.path.join(target_folder_path, file)
                         with open(encrypted_file_path, 'wb') as f:
                             f.write(encrypted_data)
-                commit_changes(f"Account '{folder}' created with encrypted license data.")
                 print(f"'{folder}' created an account with an encrypted license.")
             except Exception as e:
                 print(f"Error: {e}")
 def delete_removed_folders(existing_folders, registered_folders):
     for folder in existing_folders:
-        if folder != '.git' and folder not in registered_folders:
+        if folder not in registered_folders:
             try:
-                shutil.rmtree(os.path.join(repo, folder))
-                commit_changes(f"Deleted account '{folder}'.")
+                sudo_command = f"sudo rm -r {os.path.join(webdavdirectory, folder)}"
+                subprocess.run(sudo_command, shell=True)
                 print(f"Account '{folder}' deleted.")
             except Exception as e:
                 print(f"Error: {e}")
-def commit_changes(commit_message):
-    global token
-    try:
-        git_add = f'git -C {repo} add .'
-        git_commit = f'git -C {repo} commit -m "{commit_message}"'
-        git_push = 'git -C ' + repo + ' push https://' + token + '@' + url
-        os.system(git_add)
-        os.system(git_commit)
-        os.system(git_push)
-        print("Changes committed and pushed successfully through Git.")
-    except Exception as e:
-        print(f"Error: {e}")
-def monitor(registered_accounts, repo_path):
+def monitor(registered_accounts, var_path):
     while True:
-        existing_folders = [folder for folder in os.listdir(repo_path) if os.path.isdir(os.path.join(repo_path, folder)) and folder != '.git']
+        existing_folders = [folder for folder in os.listdir(var_path) if os.path.isdir(os.path.join(var_path, folder)) and folder != '.git']
         registered_folders = [folder for folder in os.listdir(registered_accounts) if os.path.isdir(os.path.join(registered_accounts, folder))]
         process_new_folders(registered_folders)
         delete_removed_folders(existing_folders, registered_folders)
@@ -118,19 +79,16 @@ def load_config():
         config = json.load(config_file)
     return config
 config = load_config()
-username = config.get('GITUSERNAME', ' ')
-licensestorage_repo = config.get('GITSTORAGEREPO', ' ')
-token = config.get('GITPAT', ' ')
-pubkeylink = config.get('netlifyURL', ' ')
-pubkeylink = pubkeylink if pubkeylink.endswith('/') else pubkeylink + '/'
-if token == ' ' or username == ' ' or licensestorage_repo == ' ' or pubkeylink == ' ':
+publiclink = config.get('webdavLink', ' ')
+publiclink = publiclink if publiclink.endswith('/') else publiclink + '/'
+webdavdirectory = config.get('webdavDir', ' ')
+webdavdirectory = webdavdirectory if webdavdirectory.endswith('/') else webdavdirectory + '/'
+if publiclink == ' ' or webdavdirectory == ' ':
     print("Missing configuration. Please check your config.json file.")
-url = f'github.com/{username}/{licensestorage_repo}'
-repo = clonerepoat + licensestorage_repo
 identifier = b'3iDdjV4wARLuGZaPN9_E-hqHT0O8Ibiju293QLmCsgo='
 registered_accounts = 'registered'  
 if not os.path.exists('identifiers.txt'):
-    backupfile = glob.glob('/Users/' + os.getlogin() + '/Documents/' + 'akodidentifiers-backup-*.txt')
+    backupfile = glob.glob('/home/' + os.getlogin() + '/Documents/' + 'akodidentifiers-backup-*.txt')
     if backupfile:
         for backupfile in backupfile:
             if os.path.exists(backupfile):
@@ -147,7 +105,7 @@ if not os.path.exists('identifiers.txt'):
     if not bytes(''.join(chr(array) for array in [51, 105, 68, 100, 106, 86, 52, 119, 65, 82, 76, 117, 71, 90, 97, 80, 78, 57, 95, 69, 45, 104, 113, 72, 84, 48, 79, 56, 73, 98, 105, 106, 117, 50, 57, 51, 81, 76, 109, 67, 115, 103, 111, 61]), 'utf-8') == identifier: print("It seems that you've edited the identifier from the default identifier key! Only change the identifier if you know what you are doing, as it will make validating licenses impossible with AKoDAuth. Learn more at https://github.com/tagoworks/akod") and exit()
     with open('identifiers.txt', 'w') as f:
         fernet = Fernet(identifier)
-        pubkey = fernet.encrypt(pubkeylink.encode()).decode()
+        pubkey = fernet.encrypt(publiclink.encode()).decode()
         f.write("---- This auto generated file contains very sensitive strings - Do not share them with anyone - https://github.com/tagoWorks/akod/wiki/AKoD-Encryption-Variables ----\n\n")
         f.write('PRIVATE KEY IDENTIFIER\n')
         f.write(privkey)
@@ -155,7 +113,7 @@ if not os.path.exists('identifiers.txt'):
         f.write(pubkey)
         f.write("\n\n---- This auto generated file contains very sensitive strings - Do not share them with anyone - https://github.com/tagoWorks/akod/wiki/AKoD-Encryption-Variables ----\n\n")
         f.close()
-    with open ('/Users/' + os.getlogin() + '/Documents/akodidentifiers-backup-' + time.strftime("%d-%m-%Y-%H-%M-%S") + '.txt', 'w') as f:
+    with open ('/home/' + os.getlogin() + '/Documents/' + 'akodidentifiers-backup-' + time.strftime("%d-%m-%Y-%H-%M-%S") + '.txt', 'w') as f:
         f.write("---- This auto generated file contains very sensitive strings - Do not share them with anyone - https://github.com/tagoWorks/akod/wiki/AKoD-Encryption-Variables ----\n\n")
         f.write('PRIVATE KEY IDENTIFIER\n')
         f.write(privkey)
@@ -165,8 +123,6 @@ if not os.path.exists('identifiers.txt'):
         f.close()
     print("Generated identifiers. Please do not share it with anyone. It is recommended that you do not regenerate it.")
     exit()
-if not os.path.exists(repo):
-    clone(url, token)
 with open('identifiers.txt', 'r') as f:
     iv = os.urandom(16)
     lines = f.readlines()
@@ -176,6 +132,18 @@ with open('identifiers.txt', 'r') as f:
             break
 if not os.path.exists(registered_accounts):
     os.mkdir(registered_accounts)
+    subprocess.run(['sudo', 'chown', 'www-data:www-data', '/var/www/html/'])
+    if os.path.exists('/var/www/html/index.html'):
+        subprocess.run(['sudo', 'rm', '/var/www/html/index.html'])
+    url = 'https://cdn.tago.works/apps/akod/webpage/ubu.html'
+    response = requests.get(url)
+    content = response.text
+    new = content.replace('https://githublink.com/', f'{publiclink}')
+    with subprocess.Popen(['sudo', 'tee', '/var/www/html/index.html'], stdin=subprocess.PIPE) as f:
+        f.stdin.write(new.encode('utf-8'))
+    exit()
+if os.getuid() != 0:
+    print("This script requires administrative privileges. Please run it with sudo.")
     exit()
 print("\n\nWatching account folder...")
-monitor(registered_accounts, repo)
+monitor(registered_accounts, webdavdirectory)
